5fed62e00d6e0787ede89533e1fa38094a31fd2a
Getulio Silva <getulio.r.da.silva@avande.com>
2021-06-21 09:10:57 -0300

    Add new rep, create Dto, automapp e new interfaces

--- src/Avanade.SubTCSE.Projeto/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ src/Avanade.SubTCSE.Projeto/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -13,8 +13,8 @@
             RoleName = roleName;
         }
 
-        public string Id { get; private set; }
+        public string Id { get; init; }
 
-        public string RoleName { get; private set; }
+        public string RoleName { get; init; }
     }
 }
