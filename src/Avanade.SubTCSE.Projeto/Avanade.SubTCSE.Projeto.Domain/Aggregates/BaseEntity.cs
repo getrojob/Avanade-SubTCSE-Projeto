35c0a6f28a5e73d947af99753b8ead7af38dfc9b
Getulio Silva <getulio.r.da.silva@avande.com>
2021-06-29 08:53:35 -0300

    Update: BaseEntity

--- src/Avanade.SubTCSE.Projeto/Avanade.SubTCSE.Projeto.Domain/Aggregates/BaseEntity.cs
+++ src/Avanade.SubTCSE.Projeto/Avanade.SubTCSE.Projeto.Domain/Aggregates/BaseEntity.cs
@@ -1,7 +1,18 @@
-﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates
+﻿using System;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates
 {
     public record BaseEntity<Tid>
     {
         public Tid Id { get; set; }
+
+        private DateTime? _createAt;
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = (value == null ? DateTime.UtcNow : value); }
+        }
+        public DateTime? UpdateAt { get; set; }
+
     }
 }
