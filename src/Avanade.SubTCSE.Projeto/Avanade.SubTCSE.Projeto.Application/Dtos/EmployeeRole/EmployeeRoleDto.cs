109b6664d8e4b200fa70a0a5946b5fd6f40a4a01
Getulio Silva <getulio.r.da.silva@avande.com>
2021-06-29 09:02:46 -0300

    Equalizando projeto

--- src/Avanade.SubTCSE.Projeto/Avanade.SubTCSE.Projeto.Application/Dtos/EmployeeRole/EmployeeRoleDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole
-{
-    public class EmployeeRoleDto
-    {
-        public string Identificador { get; set; }
-        public string Cargo { get; set; }
-    }
-}
--- src/Avanade.SubTCSE.Projeto/Avanade.SubTCSE.Projeto.Application/Interfaces/EmployeeRole/IEmployeeRoleAppService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avanade.SubTCSE.Projeto.Application.Dtos.EmployeeRole;
-
-namespace Avanade.SubTCSE.Projeto.Application.Interfaces.EmployeeRole
-{
-    public interface IEmployeeRoleAppService
-    {
-        void AddEmployeeRole(EmployeeRoleDto employeeRoleDto);
-    }
-}
