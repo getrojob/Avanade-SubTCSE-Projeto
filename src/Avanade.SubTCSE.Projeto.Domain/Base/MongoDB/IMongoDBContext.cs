899f496284d416df857d77c6f86eb02d8b4faa3f
Getulio Silva <getulio.r.da.silva@avande.com>
2021-07-05 08:59:03 -0300

    Add Oraganização e criação de interfaces e nuget MongoDB e seu context

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Base/MongoDB/IMongoDBContext.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Driver;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Base.MongoDB
+{
+    public interface IMongoDBContext
+    {
+        IMongoCollection<TEntity> GetCollection<TEntity>(string collection);
+    }
+}
+
+//ACID
